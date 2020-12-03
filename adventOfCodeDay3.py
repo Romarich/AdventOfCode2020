10a4ef13a1c55627828b2fbf2784c65273305a0c
Romaric Honorez <romaric.honorez.etu@univ-lille.fr>
2020-12-03 17:26:11 +0100

    adventOfCodeDay3

--- adventOfCodeDay3.py
+++ adventOfCodeDay3.py
@@ -1,12 +1,77 @@
 #Part 1 AdventOfCode Day 3
 def day3AdventOfCodePart1():
 	f = open("input/inputDay3.txt","r")
-	return 1
+	valeurARenvoyer = 0
+	indice = 0
+	for x in f:
+		if(indice > (len(x)-2)):
+			indice = indice - (len(x)-1)
+		if(x[indice] == '#'):
+			valeurARenvoyer += 1
+		indice += 3
+	return valeurARenvoyer
 	
 #Part 2 AdventOfCode Day 3
 def day3AdventOfCodePart2():
 	f = open("input/inputDay3.txt","r")
-	return 1
+	valeurARenvoyer = 0
+	valeurFinale = 1
+	indice = 0
+	for x in f:
+		if(indice > (len(x)-2)):
+			indice = indice - (len(x)-1)
+		if(x[indice] == '#'):
+			valeurARenvoyer += 1
+		indice += 1
+
+	valeurFinale *= valeurARenvoyer
+	valeurARenvoyer = 0
+	indice = 0
+	f = open("input/inputDay3.txt","r")
+	for x in f:
+		if(indice > (len(x)-2)):
+			indice = indice - (len(x)-1)
+		if(x[indice] == '#'):
+			valeurARenvoyer += 1
+		indice += 3
+	valeurFinale *= valeurARenvoyer
+	valeurARenvoyer = 0
+	indice = 0
+	f = open("input/inputDay3.txt","r")
+	for x in f:
+		if(indice > (len(x)-2)):
+			indice = indice - (len(x)-1)
+		if(x[indice] == '#'):
+			valeurARenvoyer += 1
+		indice += 5
+	valeurFinale *= valeurARenvoyer
+	valeurARenvoyer = 0
+	indice = 0
+	f = open("input/inputDay3.txt","r")
+	for x in f:
+		if(indice > (len(x)-2)):
+			indice = indice - (len(x)-1)
+		if(x[indice] == '#'):
+			valeurARenvoyer += 1
+		indice += 7
+	valeurFinale *= valeurARenvoyer
+	valeurARenvoyer = 0
+	indice = 0
+	valeurProfondeur = 0
+	f = open("input/inputDay3.txt","r")
+	for x in f:
+		
+		if(valeurProfondeur % 2 == 0):
+			if(indice > (len(x)-2)):
+				indice = indice - (len(x)-1)
+			if(x[indice] == '#'):
+				valeurARenvoyer += 1
+			indice += 1
+		valeurProfondeur += 1
+	valeurFinale *= valeurARenvoyer
+	valeurARenvoyer = 0
+	indice = 0
+	return valeurFinale
 	
 print(day3AdventOfCodePart1())
 print(day3AdventOfCodePart2())
---
+++ adventOfCodeDay4.py
@@ -0,0 +1,19 @@
+#Part 1 AdventOfCode Day 4
+def day4AdventOfCodePart1():
+	f = open("input/inputDay4.txt","r")
+	int valeurARenvoyer = 0
+	for x in f:
+
+	return 1
+	
+#Part 2 AdventOfCode Day 4
+def day4AdventOfCodePart2():
+	f = open("input/inputDay4.txt","r")
+	int valeurARenvoyer = 0
+	for x in f:
+
+	return 1
+	
+print(day4AdventOfCodePart1())
+print(day4AdventOfCodePart2())
+
